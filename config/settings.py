87c10e4878a1118d44f361bd3e852e5f81f9ff60
tjdud22 <tjdud2249@gmail.com>
2023-07-31 21:03:41 +0900

    setting

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "ocrdb",    #데이터베이스명
         "USER": "root",
-        "PASSWORD": "my1234",
+        "PASSWORD": "root",
         "HOST": "localhost",
         "PORT": "3306",
     }
